806f3298daed4ed08869ceb885f088624db5e600
Derrickhho <derrickhho@gmail.com>
2017-03-07 18:06:51 -0500

    updated!!

---
+++ final project/specs/derrick_plots.js
@@ -0,0 +1,67 @@
+// script for NYC map
+
+var svgMap = d3.select("#popularLoc");
+var uberPickups = [[-73.715, 40.7],[-73.715, 40.5],[-74.4533, 40.4996]];
+var highlightPoint = [[-73.976368, 40.758637]]
+
+d3.csv("data/uber-raw-data.csv.csv", function (data) {
+
+  data.forEach(function(d) {
+    d.Lat = +d.Lat;
+    d.Lon = +d.Lon;
+  })
+
+  uberPickups = data;
+
+  var points = new Array(0);
+  uberPickups.forEach(function(tree) {
+    var a = [tree.Lon, tree.Lat];
+    // a[1] = tree.species;
+    points.push(a);
+  });
+
+  //Making the map
+  d3.json("data/newyork.geojson", function (data) {
+
+    var group = svgMap.selectAll(".area")
+      .data(data.features)
+      .enter()
+      .append("g")
+
+    var projection = d3.geoMercator()
+      .scale(150000)
+      .center([-73.87, 40.76]);
+    var path = d3.geoPath().projection(projection);
+
+    var areas = group.append("path")
+      .attr("d", path)
+      .attr("class", "area")
+      .attr("fill", "#8ADAC1")
+
+    //Making the circles
+    var circles = svgMap.selectAll(".circles").data(points);
+    var highlights = svgMap.selectAll(".highlights").data(highlightPoint);
+
+    circles.enter().append("circle")
+      .attr("class", "circles")
+      .attr("r", 3)
+      .attr("cx", function(d) {
+        //console.log(projection(d));
+        return projection(d)[0]; })
+      .attr("cy", function(d) {
+        return projection(d)[1]; })
+      .attr("fill", "white")
+      .attr("opacity", "0.1");
+
+    highlights.enter().append("circle")
+      .attr("class", "highlights")
+      .attr("r", 38)
+      .attr("stroke", "#ffffcc")
+      .attr("stroke-width", 3)
+      .attr("fill", "none")
+      .attr("cx", function(d) {
+        return projection(d)[0]; })
+      .attr("cy", function(d) {
+        return projection(d)[1]; })
+  });
+});
---
+++ final project/specs/lini_plots.js
@@ -0,0 +1,221 @@
+// script for dot plot
+
+var svg = d3.select("#population_pickup");
+svg.append("line")
+.style("stroke","black")
+.attr("x1", 50)
+.attr("y1", 350)
+.attr("x2", 350)
+.attr("y2", 350);
+svg.append("text")
+.text("population(person)")
+.attr("x",305)
+.attr("y",360)
+.attr("font-size",10);
+svg.append("line")
+.style("stroke","black")
+.attr("x1", 50)
+.attr("y1", 350)
+.attr("x2", 50)
+.attr("y2", 50);
+svg.append("text")
+.text("Uber Pickup Amount(times)")
+.attr("x",50)
+.attr("y",40)
+.attr("font-size",10);
+
+//compare pickup amount to area
+var svg2 = d3.select("#area_pickup");
+svg2.append("line")
+.style("stroke","black")
+.attr("x1", 50)
+.attr("y1", 350)
+.attr("x2", 350)
+.attr("y2", 350);
+svg2.append("text")
+.text("Land area(square miles)")
+.attr("x",290)
+.attr("y",360)
+.attr("font-size",10);
+svg2.append("line")
+.style("stroke","black")
+.attr("x1", 50)
+.attr("y1", 350)
+.attr("x2", 50)
+.attr("y2", 50);
+svg2.append("text")
+.text("Uber Pickup Amount(times)")
+.attr("x",50)
+.attr("y",40)
+.attr("font-size",10);
+
+//compare pickup amount to Wealth
+var svg3 = d3.select("#wealth_pickup");
+svg3.append("line")
+.style("stroke","black")
+.attr("x1", 50)
+.attr("y1", 350)
+.attr("x2", 350)
+.attr("y2", 350);
+svg3.append("text")
+.text("Avarage Income($)")
+.attr("x",295)
+.attr("y",360)
+.attr("font-size",10);
+svg3.append("line")
+.style("stroke","black")
+.attr("x1", 50)
+.attr("y1", 350)
+.attr("x2", 50)
+.attr("y2", 50);
+svg3.append("text")
+.text("Uber Pickup Amount(times)")
+.attr("x",50)
+.attr("y",40)
+.attr("font-size",10);
+// Define variables outside the scope of the callback function.
+var boroughData;
+
+// This function will be applied to all rows. Select three columns, change names, and convert strings to numbers.
+function parseLine (line) {
+	return {
+		Borough: line["BoroughName"],
+		Pickup: Number(line["UberPickup"]),
+		PickupPercent: Number(line["PickupPercent"]),
+		Wealth: Number(line["MeanIncome"]),
+		Population: Number(line["Population"]),
+		LandArea: Number(line["LandArea"])
+	};
+}
+
+d3.tsv("data/borough.tsv", parseLine, function (error, data) {
+	boroughData = data;
+	var popExtent = d3.extent(boroughData,
+		function (d) { return d.Population; });
+
+	var popScale = d3.scaleLog()
+	.domain(popExtent)
+	.range([100, 300]);
+
+	var wealthExtent = d3.extent(boroughData,
+		function (d) { return d.Wealth; });
+
+	var wealthScale = d3.scaleLog()
+	.domain(wealthExtent)
+	.range([100, 300]);
+
+	var areaExtent = d3.extent(boroughData,
+		function (d) { return d.LandArea; });
+
+	var areaScale = d3.scaleLog()
+	.domain(areaExtent)
+	.range([100, 300]);
+
+	var pickupExtent = d3.extent(boroughData,
+		function (d) { return d.Pickup; });
+
+	var pickupScale = d3.scaleLog()
+	.domain(pickupExtent)
+	.range([300, 100]);
+
+
+	popCompareData = boroughData.filter(function (d) { return d.Population && d.Pickup; });
+
+
+
+	popCompareData.forEach(function (borough) {
+
+		svg.append("circle")
+		.attr("cx", popScale(borough.Population))
+		.attr("cy", pickupScale(borough.Pickup))
+		.attr("r", 10)
+		.style("fill", "#238443")
+		.style("opacity", 0.5)
+		});
+
+	popCompareData.forEach(function (borough) {
+		svg.append("text")
+		.text(borough.Borough)
+		.attr("x",popScale(borough.Population)-45)
+		.attr("y",pickupScale(borough.Pickup))
+		.attr("font-size",15);
+		});
+
+		popCompareData.forEach(function (borough) {
+			svg.append("text")
+			.text(borough.Population+" person")
+			.attr("x",popScale(borough.Population))
+			.attr("y",pickupScale(borough.Pickup)+10)
+			.attr("font-size",11)
+			.style("fill","#341dff");
+			});
+
+		wealthCompareData = boroughData.filter(function (d) { return d.Wealth && d.Pickup; });
+
+		wealthCompareData.forEach(function (borough) {
+
+			svg3.append("circle")
+			.attr("cx", wealthScale(borough.Wealth))
+			.attr("cy", pickupScale(borough.Pickup))
+			.attr("r", 10)
+			.style("fill", "#238443")
+			.style("opacity", 0.5)
+			});
+
+
+		wealthCompareData.forEach(function (borough) {
+			svg3.append("text")
+			.text(borough.Borough)
+			.attr("x",wealthScale(borough.Wealth)+5)
+			.attr("y",pickupScale(borough.Pickup)-5)
+			.attr("font-size",15);
+			});
+
+			wealthCompareData.forEach(function (borough) {
+				svg3.append("text")
+				.text("$"+borough.Wealth)
+				.attr("x",wealthScale(borough.Wealth)-10)
+				.attr("y",pickupScale(borough.Pickup)+6)
+				.attr("font-size",11)
+				.style("fill","#341dff");
+				});
+
+			areaCompareData = boroughData.filter(function (d) { return d.LandArea && d.Pickup; });
+
+			areaCompareData.forEach(function (borough) {
+
+				svg2.append("circle")
+				.attr("cx", areaScale(borough.LandArea))
+				.attr("cy", pickupScale(borough.Pickup))
+				.attr("r", 10)
+				.style("fill", "#238443")
+				.style("opacity", 0.5)
+				});
+
+
+			areaCompareData.forEach(function (borough) {
+				svg2.append("text")
+				.text(borough.Borough)
+				.attr("x",areaScale(borough.LandArea))
+				.attr("y",pickupScale(borough.Pickup))
+				.attr("font-size",15);
+				});
+
+				areaCompareData.forEach(function (borough) {
+					svg2.append("text")
+					.text(borough.LandArea+" m2")
+					.attr("x",areaScale(borough.LandArea))
+					.attr("y",pickupScale(borough.Pickup)+10)
+					.attr("font-size",11)
+					.style("fill","#341dff");
+					});
+
+					areaCompareData.forEach(function (borough) {
+						svg2.append("text")
+						.text(borough.Pickup+" times")
+						.attr("x",areaScale(borough.LandArea))
+						.attr("y",pickupScale(borough.Pickup)+20)
+						.attr("font-size",11)
+						.style("fill","#dc97ff");
+						});
+	});
