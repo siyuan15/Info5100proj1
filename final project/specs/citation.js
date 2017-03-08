99fa3b3e327d63adad94f03566074366830fc074
siyuan15 <sy652@cornell.edu>
2017-03-07 20:15:55 -0500

    Create citation.js

---
+++ final project/specs/citation.js
@@ -0,0 +1,8 @@
+// Reference code for line plot
+  .selectAll("text")
+    .style("text-anchor", "end")
+    .attr("dx", "-.8em")
+    .attr("dy", ".15em")
+    .attr("transform", function(d) {
+        return "rotate(-65)"
+        });
